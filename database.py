e518fc5196a51e536873bc6b6e49acc0c4bed525
Maxim <makseroshtanoff@gmail.com>
2024-06-05 15:12:05 +0300

    Фикс
    Фикс

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-conn_str = 'sqlite:///' + os.path.join(BASE_DIR, 'project.db')
+conn_str = 'sqlite:///' + os.path.join(BASE_DIR, 'project1.db')
 
 
 engine = create_engine(conn_str, echo=True)
